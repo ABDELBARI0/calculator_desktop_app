88a6c282b0dcd3319bd68735b7e7b26906a43457
haddouche abdelbari <abdelbarihaddouche@gmail.com>
2026-06-29 21:27:23 +0100

    the first version of calculator desktop application

---
+++ ui_form.py
@@ -0,0 +1,292 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.11.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QPushButton, QSizePolicy,
+    QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(241, 361)
+        self.pushButton_C = QPushButton(Widget)
+        self.pushButton_C.setObjectName(u"pushButton_C")
+        self.pushButton_C.setGeometry(QRect(0, 60, 61, 61))
+        self.pushButton_C.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(215, 215, 215);\n"
+"  border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #BEBEBE, stop: 1 #D7D7D7);\n"
+"}")
+        self.pushButton_C.setIconSize(QSize(61, 61))
+        self.pushButton_plus_mines = QPushButton(Widget)
+        self.pushButton_plus_mines.setObjectName(u"pushButton_plus_mines")
+        self.pushButton_plus_mines.setGeometry(QRect(60, 60, 61, 61))
+        self.pushButton_plus_mines.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(215, 215, 215);\n"
+"  border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #BEBEBE, stop: 1 #D7D7D7);\n"
+"}")
+        self.pushButton_plus_mines.setIconSize(QSize(61, 61))
+        self.pushButton_persontage = QPushButton(Widget)
+        self.pushButton_persontage.setObjectName(u"pushButton_persontage")
+        self.pushButton_persontage.setGeometry(QRect(120, 60, 61, 61))
+        self.pushButton_persontage.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(215, 215, 215);\n"
+"  border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #BEBEBE, stop: 1 #D7D7D7);\n"
+"}")
+        self.pushButton_persontage.setIconSize(QSize(61, 61))
+        self.pushButton_9 = QPushButton(Widget)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(120, 120, 61, 61))
+        self.pushButton_9.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_9.setIconSize(QSize(61, 61))
+        self.pushButton_8 = QPushButton(Widget)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(60, 120, 61, 61))
+        self.pushButton_8.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_8.setIconSize(QSize(61, 61))
+        self.pushButton_7 = QPushButton(Widget)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(0, 120, 61, 61))
+        self.pushButton_7.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_7.setIconSize(QSize(61, 61))
+        self.pushButton_6 = QPushButton(Widget)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        self.pushButton_6.setGeometry(QRect(120, 180, 61, 61))
+        self.pushButton_6.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_6.setIconSize(QSize(61, 61))
+        self.pushButton_5 = QPushButton(Widget)
+        self.pushButton_5.setObjectName(u"pushButton_5")
+        self.pushButton_5.setGeometry(QRect(60, 180, 61, 61))
+        self.pushButton_5.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_5.setIconSize(QSize(61, 61))
+        self.pushButton_4 = QPushButton(Widget)
+        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.pushButton_4.setGeometry(QRect(0, 180, 61, 61))
+        self.pushButton_4.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_4.setIconSize(QSize(61, 61))
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(0, 0, 241, 61))
+        self.label.setStyleSheet(u"QLabel {\n"
+"  qproperty-alignment: 'AlignVCenter | AlignRight';\n"
+"  border: 1px solid gray;\n"
+"}\n"
+"\n"
+"background-color : white;")
+        self.pushButton_division = QPushButton(Widget)
+        self.pushButton_division.setObjectName(u"pushButton_division")
+        self.pushButton_division.setGeometry(QRect(180, 60, 61, 61))
+        self.pushButton_division.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(255, 151, 57);\n"
+"  color: white; \n"
+"  border: 1px solid gray;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+"}")
+        self.pushButton_division.setIconSize(QSize(61, 61))
+        self.pushButton_multiplication = QPushButton(Widget)
+        self.pushButton_multiplication.setObjectName(u"pushButton_multiplication")
+        self.pushButton_multiplication.setGeometry(QRect(180, 120, 61, 61))
+        self.pushButton_multiplication.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(255, 151, 57);\n"
+"  color: white; \n"
+"  border: 1px solid gray;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+"}")
+        self.pushButton_multiplication.setIconSize(QSize(61, 61))
+        self.pushButton_sub = QPushButton(Widget)
+        self.pushButton_sub.setObjectName(u"pushButton_sub")
+        self.pushButton_sub.setGeometry(QRect(180, 180, 61, 61))
+        self.pushButton_sub.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(255, 151, 57);\n"
+"  color: white; \n"
+"  border: 1px solid gray;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+"}")
+        self.pushButton_sub.setIconSize(QSize(61, 61))
+        self.pushButton_1 = QPushButton(Widget)
+        self.pushButton_1.setObjectName(u"pushButton_1")
+        self.pushButton_1.setGeometry(QRect(0, 240, 61, 61))
+        self.pushButton_1.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_1.setIconSize(QSize(61, 61))
+        self.pushButton_2 = QPushButton(Widget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_2.setGeometry(QRect(60, 240, 61, 61))
+        self.pushButton_2.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_2.setIconSize(QSize(61, 61))
+        self.pushButton_add = QPushButton(Widget)
+        self.pushButton_add.setObjectName(u"pushButton_add")
+        self.pushButton_add.setGeometry(QRect(180, 240, 61, 61))
+        self.pushButton_add.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(255, 151, 57);\n"
+"  color: white; \n"
+"  border: 1px solid gray;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+"}")
+        self.pushButton_add.setIconSize(QSize(61, 61))
+        self.pushButton_3 = QPushButton(Widget)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.pushButton_3.setGeometry(QRect(120, 240, 61, 61))
+        self.pushButton_3.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_3.setIconSize(QSize(61, 61))
+        self.pushButton_0 = QPushButton(Widget)
+        self.pushButton_0.setObjectName(u"pushButton_0")
+        self.pushButton_0.setGeometry(QRect(0, 300, 121, 61))
+        self.pushButton_0.setStyleSheet(u"QPushButton {\n"
+"   border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #dadbde, stop: 1 #f6f7fa);\n"
+"}")
+        self.pushButton_0.setIconSize(QSize(61, 61))
+        self.pushButton_dot = QPushButton(Widget)
+        self.pushButton_dot.setObjectName(u"pushButton_dot")
+        self.pushButton_dot.setGeometry(QRect(120, 300, 61, 61))
+        self.pushButton_dot.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(215, 215, 215);\n"
+"  border: 1px solid gray;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #BEBEBE, stop: 1 #D7D7D7);\n"
+"}")
+        self.pushButton_dot.setIconSize(QSize(61, 61))
+        self.pushButton_equal = QPushButton(Widget)
+        self.pushButton_equal.setObjectName(u"pushButton_equal")
+        self.pushButton_equal.setGeometry(QRect(180, 300, 61, 61))
+        self.pushButton_equal.setStyleSheet(u"QPushButton {\n"
+"  background-color: rgb(255, 151, 57);\n"
+"  color: white; \n"
+"  border: 1px solid gray;\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\n"
+"                                      stop: 0 #FF7832, stop: 1 #FF9739);\n"
+"}")
+        self.pushButton_equal.setIconSize(QSize(61, 61))
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        self.pushButton_C.setText(QCoreApplication.translate("Widget", u"C", None))
+        self.pushButton_plus_mines.setText(QCoreApplication.translate("Widget", u"+/-", None))
+        self.pushButton_persontage.setText(QCoreApplication.translate("Widget", u"%", None))
+        self.pushButton_9.setText(QCoreApplication.translate("Widget", u"9", None))
+        self.pushButton_8.setText(QCoreApplication.translate("Widget", u"8", None))
+        self.pushButton_7.setText(QCoreApplication.translate("Widget", u"7", None))
+        self.pushButton_6.setText(QCoreApplication.translate("Widget", u"6", None))
+        self.pushButton_5.setText(QCoreApplication.translate("Widget", u"5", None))
+        self.pushButton_4.setText(QCoreApplication.translate("Widget", u"4", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"0", None))
+        self.pushButton_division.setText(QCoreApplication.translate("Widget", u"\u00f7", None))
+        self.pushButton_multiplication.setText(QCoreApplication.translate("Widget", u"\u00d7", None))
+        self.pushButton_sub.setText(QCoreApplication.translate("Widget", u"-", None))
+        self.pushButton_1.setText(QCoreApplication.translate("Widget", u"1", None))
+        self.pushButton_2.setText(QCoreApplication.translate("Widget", u"2", None))
+        self.pushButton_add.setText(QCoreApplication.translate("Widget", u"+", None))
+        self.pushButton_3.setText(QCoreApplication.translate("Widget", u"3", None))
+        self.pushButton_0.setText(QCoreApplication.translate("Widget", u"0", None))
+        self.pushButton_dot.setText(QCoreApplication.translate("Widget", u".", None))
+        self.pushButton_equal.setText(QCoreApplication.translate("Widget", u"=", None))
+    # retranslateUi
+
